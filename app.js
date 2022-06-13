aa5a3b42fc1f515364591273358d534d4f12083e
Amy Woodworth <woodworthcommunications@gmail.com>
2022-06-13 17:58:17 -0500

    Heading opens accordion on button click.

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-const accordionHeaders = [...document.querySelectorAll('.accordion h2')];
 const accordionButtons = document.querySelectorAll('.accordion__trigger');
 
 accordionButtons.forEach(button => {
-  const number = button
-    .getAttribute('id')
-    .split('_')
-    .pop();
-
+  const expanded = button.getAttribute('aria-expanded');
+  const number = button.getAttribute('id').split('_').pop();
   const associatedContent = document.getElementById(
     `accordion__content_${number}`
   );
+
+  button.addEventListener('click', () => {
+    console.log('You clicked me!');
+    button.parentNode.classList.add('active');
+    button.setAttribute('aria-expanded', true);
+    associatedContent.setAttribute('aria-hidden', false);
+    associatedContent.removeAttribute('hidden');
+  });
 });
-
-// button.addEventListener('click', () => {
-//   button.classList.toggle()
-// });
-
-
